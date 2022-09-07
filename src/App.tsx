6cff17ba8d7d053627d41d38c15c413793204642
kielon <mosadowski23@gmail.com>
2022-09-07 21:06:39 +0200

    added search bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import { MainPage } from "./MainPage/components/MainPage";
 import { Login } from "./Olx/Login/Components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
---
+++ src/MainPage/components/MainPage.tsx
@@ -0,0 +1,9 @@
+import { SearchBar } from "./SearchBar";
+
+export const MainPage = () => {
+  return (
+    <div className="mainpage-wrapper">
+      <SearchBar />
+    </div>
+  );
+};
