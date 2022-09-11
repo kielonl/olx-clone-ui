af6d82958413d731dc46f2ef5e7bf1715e97f00d
kielon <mosadowski23@gmail.com>
2022-09-11 17:19:55 +0200

    added categories section

---
+++ src/MainPage/components/Categories.tsx
@@ -0,0 +1,30 @@
+import "../styles/Categories.scss";
+import samochud from "../../assets/samochud.webp";
+import { Category } from "./Category";
+
+export const Categories = () => {
+  return (
+    <div className="categories-wrapper">
+      <h1>Kategorie główne</h1>
+      <div className="categories-box">
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+        <Category title="samochud" image={samochud} />
+      </div>
+    </div>
+  );
+};
--- src/MainPage/components/MainPage.tsx
+++ src/MainPage/components/MainPage.tsx
@@ -1,9 +1,11 @@
+import { Categories } from "./Categories";
 import { SearchBar } from "./SearchBar";
 
 export const MainPage = () => {
   return (
     <div className="mainpage-wrapper">
       <SearchBar />
+      <Categories />
     </div>
   );
 };
