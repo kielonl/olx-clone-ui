eab06c74181d56b22f2dcc6871b6300f497480dc
kielon <mosadowski23@gmail.com>
2022-09-16 17:19:07 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import { Navbar } from "./MainPage/NavBar/components/Navbar";
 import { Login } from "./Olx/Login/Components/Login";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
