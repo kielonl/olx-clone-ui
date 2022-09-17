6df15eaaa6d92945c3e2c651430f564630fa0813
kielon <mosadowski23@gmail.com>
2022-09-17 19:50:22 +0200

    added useApiRequest hook

---
+++ src/api/makeRequest.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const urlInternal = process.env.REACT_APP_API;
+export const makeRequest = async (
+  method: string,
+  url: string,
+  data: string[] | undefined
+) => {
+  const request = await axios({
+    method: method,
+    url: `${urlInternal}${url}`,
+    data: data,
+  });
+  return request;
+};
