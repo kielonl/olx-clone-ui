36e3d31318500ffe2b6a9d8cea96d552dc497b7f
kielon <mosadowski23@gmail.com>
2022-09-21 22:54:22 +0200

    changed modal styling

--- src/AddingOfferPage/components/Header/components/Category/components/Modal.tsx
+++ src/AddingOfferPage/components/Header/components/Category/components/Modal.tsx
@@ -1,13 +1,30 @@
 import { FC } from "react";
+import { UseApiRequest } from "../../../../../../hooks/useApiRequest";
 import { Categories } from "../../../../../../MainPage/components/Categories/components/Categories";
+import { Category } from "../../../../../../MainPage/components/Categories/components/Category";
 import "../styles/Modal.scss";
 
+interface CategoriesMap {
+  name: string;
+  color: string;
+  image: string;
+}
 interface ModalProps {
   modalState: boolean;
   openModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Modal: FC<ModalProps> = ({ modalState = false, openModal }) => {
+  const { response, loading, error } = UseApiRequest("get", "/category");
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error</div>;
+
+  const renderCategories = response.data.result.map(
+    (categoryInfo: CategoriesMap) => {
+      return <Category title={categoryInfo.name} image={categoryInfo.image} />;
+    }
+  );
+
   return (
     <div
       className={`modal-wrapper ${modalState ? "modal-open" : "modal-close"}`}
@@ -18,7 +35,7 @@ export const Modal: FC<ModalProps> = ({ modalState = false, openModal }) => {
       >
         X
       </div>
-      <Categories />
+      <div className="modal-categories">{renderCategories}</div>
     </div>
   );
 };
