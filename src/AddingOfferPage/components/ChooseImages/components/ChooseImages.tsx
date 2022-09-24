f882306e4379fa9e25454175c3d9524b621d09b5
kielon <mosadowski23@gmail.com>
2022-09-24 23:38:04 +0200

    user can now add images to images array

--- src/AddingOfferPage/components/ChooseImages/components/ChooseImages.tsx
+++ src/AddingOfferPage/components/ChooseImages/components/ChooseImages.tsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import "../styles/ChooseImages.scss";
 import { ImageInput } from "./ImageInput";
 
+interface ImagesProps {
+  id: number;
+  url: string;
+}
+
 export const ChooseImages = () => {
+  const [images, setImages] = useState<ImagesProps[]>([]);
+  console.log(images);
+
   return (
     <div className="choose-images-wrapper">
       <div className="choose-images-field">
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
-        <ImageInput />
+        <ImageInput setImage={setImages} images={images} id={0} />
+        <ImageInput setImage={setImages} images={images} id={1} />
+        <ImageInput setImage={setImages} images={images} id={2} />
+        <ImageInput setImage={setImages} images={images} id={3} />
+        <ImageInput setImage={setImages} images={images} id={4} />
+        <ImageInput setImage={setImages} images={images} id={5} />
+        <ImageInput setImage={setImages} images={images} id={6} />
+        <ImageInput setImage={setImages} images={images} id={7} />
       </div>
     </div>
   );
--- src/AddingOfferPage/components/ChooseImages/components/ImageInput.tsx
+++ src/AddingOfferPage/components/ChooseImages/components/ImageInput.tsx
@@ -5,25 +5,66 @@ const imagePlaceholder = process.env.REACT_APP_IMAGE_PLACEHOLDER;
 
 interface ImageInputProps {
   image?: string | undefined;
+  images: any;
+  setImage: React.Dispatch<any>; //change type
+  id: number;
 }
 
-export const ImageInput: FC<ImageInputProps> = ({ image }) => {
+export const ImageInput: FC<ImageInputProps> = ({
+  image,
+  images,
+  setImage,
+  id,
+}) => {
+  const updateItem = (event: any, id: number) => {
+    const newItems = images.map((img: any) => {
+      if (id === img.id) {
+        return { ...img, url: event.target.value };
+      }
+      return img;
+    });
+    setImage(newItems);
+    console.log(images);
+  };
+
+  const sendFile = (e: any, id: number) => {
+    // change type
+    const [file] = e.target.files;
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (images.length === 0) {
+        return setImage([...images, { id: id, url: reader.result }]);
+      }
+      images.find((img: any) => {
+        if (id === img.id) {
+          return updateItem(e, id);
+        }
+        return setImage([...images, { id: id, url: reader.result }]);
+      });
+    };
+
+    //wyglada jak chujnia, zmienic to
+  };
+
   return (
-    <label
-      className="custom-file-input"
-      style={{
-        backgroundImage: `url(${
-          image === undefined ? imagePlaceholder : image
-        })`,
-      }}
-    >
-      <input type="file" />
-      <span
-        onClick={(e) => console.log(e.target)}
-        className={image !== undefined ? "image-added" : "image-not-added"}
+    <>
+      <label
+        className="custom-file-input"
+        style={{
+          backgroundImage: `url(${
+            image === undefined ? imagePlaceholder : image
+          })`,
+        }}
       >
-        Dodaj zdjecie
-      </span>
-    </label>
+        <input type="file" onChange={(e) => sendFile(e, id)} />
+
+        <span
+          className={image !== undefined ? "image-added" : "image-not-added"}
+        >
+          Dodaj zdjecie
+        </span>
+      </label>
+    </>
   );
 };
