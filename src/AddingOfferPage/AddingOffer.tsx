97f9130e56a6c821b6153d0012a4066c0422fd7e
kielon <mosadowski23@gmail.com>
2022-09-26 22:50:09 +0200

    Revert "added description component"
    This reverts commit 4bc90322673c67e70d2c5f39219c9a1e010ab774.

--- src/AddingOfferPage/AddingOffer.tsx
+++ src/AddingOfferPage/AddingOffer.tsx
@@ -1,18 +1,3 @@
-import { useState } from "react";
-import { Footer } from "../MainPage/components/Footer/components/Footer";
-import { Navbar } from "../MainPage/components/NavBar/components/Navbar";
-import { Description } from "./components/Description/components/Description";
-
 export const AddingOffer = () => {
-  const [description, setDescription] = useState<string>("");
-  return (
-    <div
-      className="adding-offer-wrapper"
-      style={{ backgroundColor: "#f2f4f5" }}
-    >
-      <Navbar />
-      <Description setDescription={setDescription} description={description} />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 };
