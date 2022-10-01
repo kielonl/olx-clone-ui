3dd944921945ed1d026977ce34694bb6d83bfce5
kielon <mosadowski23@gmail.com>
2022-10-01 19:04:04 +0200

    image inputs are now rendered with loop

--- src/AddingOfferPage/components/ChooseImages/components/ChooseImages.tsx
+++ src/AddingOfferPage/components/ChooseImages/components/ChooseImages.tsx
@@ -5,18 +5,13 @@ import { ImageInput } from "./ImageInput";
 
 export const ChooseImages = () => {
   const [images, setImages] = useState<ImagesProps[]>([]);
-
+  console.log(images);
   return (
     <div className="choose-images-wrapper">
       <div className="choose-images-field">
-        <ImageInput setImage={setImages} images={images} id={0} />
-        <ImageInput setImage={setImages} images={images} id={1} />
-        <ImageInput setImage={setImages} images={images} id={2} />
-        <ImageInput setImage={setImages} images={images} id={3} />
-        <ImageInput setImage={setImages} images={images} id={4} />
-        <ImageInput setImage={setImages} images={images} id={5} />
-        <ImageInput setImage={setImages} images={images} id={6} />
-        <ImageInput setImage={setImages} images={images} id={7} />
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((id) => {
+          return <ImageInput setImage={setImages} images={images} id={id} />;
+        })}
       </div>
     </div>
   );
